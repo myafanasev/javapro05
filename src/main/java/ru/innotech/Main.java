aac380ef7aacd0da4e47abbf32a07ea682d152fc
Afanasev Mikhail <miafanasev@gmail.com>
2024-11-23 01:21:57 +0700

    Добавлен SpringBoot

--- src/main/java/ru/innotech/Main.java
+++ src/main/java/ru/innotech/Main.java
@@ -1,16 +1,22 @@
 package ru.innotech;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cglib.proxy.Dispatcher;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.Connection;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext ctx = new AnnotationConfigApplicationContext("ru.innotech");
+        ApplicationContext ctx = SpringApplication.run(Main.class);
+        //System.out.println(ctx.getBean(DispatcherServlet));
+        //        ApplicationContext ctx = new AnnotationConfigApplicationContext("ru.innotech");
         TransformApplication transformApplication = ctx.getBean(TransformApplication.class);
-        Connection connection = ctx.getBean(Connection.class);
-        transformApplication.run();
+//        Connection connection = ctx.getBean(Connection.class);
+      transformApplication.run();
     }
 }
 
