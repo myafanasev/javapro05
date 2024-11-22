c4613041fd4f380d03a46b54f02f07ac1912e16e
Afanasev Mikhail <miafanasev@gmail.com>
2024-11-23 01:18:56 +0700

    Параметры соединения теперь хранятся в настройках

--- src/main/java/ru/innotech/connection/DataSource.java
+++ src/main/java/ru/innotech/connection/DataSource.java
@@ -4,6 +4,8 @@ package ru.innotech.connection;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
@@ -15,10 +17,12 @@ import java.sql.SQLException;
 public class DataSource {
     private  static HikariConfig config = new HikariConfig();
     private static HikariDataSource ds;
-    static {
-        config.setJdbcUrl("jdbc:postgresql://localhost:5432/javapro04");
-        config.setUsername("postgres");
-        config.setPassword("1234");
+
+    @Autowired
+    public DataSource(@Value("${datasource.url}") String url, @Value("${datasource.username}")String username, @Value("${datasource.password}")String password) {
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
         ds = new HikariDataSource(config);
     }
 
